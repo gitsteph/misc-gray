e7b6e323c32222fde4c1af85168907cce0eec9ab
Stephanie H. Andrews <byronic.string@gmail.com>
2018-09-27 10:24:32 -0700

    initial commit; adding random quote picker

---
+++ random-quote-gen/sketch.js
@@ -0,0 +1,26 @@
+var quotesArray;
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function setup() {
+	quotesArray = [
+		["You can't use up creativity.  The more you use, the more you have.", "Maya Angelou"],
+		["An idea that is not dangerous is unworthy of being called an idea at all.", "Oscar Wilde"],
+		["Vulnerability is the birthplace of innovation, creativity, and change.", "Brene Brown"],
+		["Create with the heart; build with the mind.", "Criss Jami"],
+		["Creativity is piercing the mundane to find the marvelous.", "Bill Moyers"]
+	]
+}
+
+function renderNewQuote() {
+	let randomInt = getRandomInt(0, quotesArray.length);
+	let quoteBox = document.getElementById("quoteBox");
+	quoteBox.innerHTML = quotesArray[randomInt][0];
+	let nameBox = document.getElementById("nameBox");
+	nameBox.innerHTML = "- " + quotesArray[randomInt][1];
+}
+
+setup();
