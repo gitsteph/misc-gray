ff1a93c63893c8be463dc14385a4da6e1c441c3c
Stephanie H. Andrews <byronic.string@gmail.com>
2018-09-27 10:25:43 -0700

    converting indents to spaces gh

--- random-quote-gen/sketch.js
+++ random-quote-gen/sketch.js
@@ -6,21 +6,21 @@ function getRandomInt(min, max) {
 }
 
 function setup() {
-	quotesArray = [
-		["You can't use up creativity.  The more you use, the more you have.", "Maya Angelou"],
-		["An idea that is not dangerous is unworthy of being called an idea at all.", "Oscar Wilde"],
-		["Vulnerability is the birthplace of innovation, creativity, and change.", "Brene Brown"],
-		["Create with the heart; build with the mind.", "Criss Jami"],
-		["Creativity is piercing the mundane to find the marvelous.", "Bill Moyers"]
-	]
+    quotesArray = [
+        ["You can't use up creativity.  The more you use, the more you have.", "Maya Angelou"],
+        ["An idea that is not dangerous is unworthy of being called an idea at all.", "Oscar Wilde"],
+        ["Vulnerability is the birthplace of innovation, creativity, and change.", "Brene Brown"],
+        ["Create with the heart; build with the mind.", "Criss Jami"],
+        ["Creativity is piercing the mundane to find the marvelous.", "Bill Moyers"]
+    ]
 }
 
 function renderNewQuote() {
-	let randomInt = getRandomInt(0, quotesArray.length);
-	let quoteBox = document.getElementById("quoteBox");
-	quoteBox.innerHTML = quotesArray[randomInt][0];
-	let nameBox = document.getElementById("nameBox");
-	nameBox.innerHTML = "- " + quotesArray[randomInt][1];
+    let randomInt = getRandomInt(0, quotesArray.length);
+    let quoteBox = document.getElementById("quoteBox");
+    quoteBox.innerHTML = quotesArray[randomInt][0];
+    let nameBox = document.getElementById("nameBox");
+    nameBox.innerHTML = "- " + quotesArray[randomInt][1];
 }
 
 setup();
